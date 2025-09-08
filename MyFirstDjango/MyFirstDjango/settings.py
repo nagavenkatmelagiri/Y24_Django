47dc6b969275f2b51353f2fe6d86af682a83706e
nagavenkatmelagiri <nagavenkat205@gmail.com>
2025-09-08 11:01:34 +0530

    exp1 done

--- MyFirstDjango/MyFirstDjango/settings.py
+++ MyFirstDjango/MyFirstDjango/settings.py
@@ -48,7 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+] 
 
 ROOT_URLCONF = 'MyFirstDjango.urls'
 import os
